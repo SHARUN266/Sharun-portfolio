732eb59bf98f431243e9e012f82bd6f014f072c4
SHARUN266 <sharunattari266@gmail.com>
2022-11-15 16:18:59 +0530

    gitStats

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,29 +1,23 @@
-import {
+import { Button, Flex } from "@chakra-ui/react";
 
-  Button,
-  
-  Flex,
- 
-} from "@chakra-ui/react";
-
-
-import {GrDocumentPdf } from "react-icons/gr";
-import Resume from './Sharunresume.pdf'
+import { GrDocumentPdf } from "react-icons/gr";
+import Resume from "./Sharunresume.pdf";
 export default function ClickMe() {
   return (
     <Flex mt={["3%", "4%"]}>
-      <a  href={Resume} target="_blank"   download="Sharun-Resume">
-      <Button
-                shadow={"lg"}
-                rounded={"full"}
-                size={"lg"}
-                fontWeight={"normal"}
-                px={6}
-                color={'black'}
-                leftIcon={<GrDocumentPdf h={4} w={4} color={"black"} />}
-              >
-                Resume
-              </Button></a>
+      <a href={Resume} target="_blank" download="Sharun-Resume">
+        <Button
+          shadow={"lg"}
+          rounded={"full"}
+          size={"lg"}
+          fontWeight={"normal"}
+          px={6}
+          color={"black"}
+          leftIcon={<GrDocumentPdf h={4} w={4} color={"black"} />}
+        >
+          Resume
+        </Button>
+      </a>
     </Flex>
   );
 }
