9e3a73132d8b97368c9b4145779f7ec83537d265
SHARUN266 <sharunattari266@gmail.com>
2023-01-05 21:58:53 +0530

    Practice

--- Prototype.js
+++ Prototype.js
@@ -1,32 +1,32 @@
-var myStr = "he11o123";
-//final output should be like this : ["heo",8]
+// var myStr = "he11o123";
+// //final output should be like this : ["heo",8]
 
-let bag = "";
-let sum = 0;
-for (let i = 0; i < myStr.length; i++) {
-  if (isNaN(myStr[i])) {
-    bag += myStr[i];
-  } else {
-    sum += Number(myStr[i]);
-  }
-}
-//console.log(bag,sum)
+// let bag = "";
+// let sum = 0;
+// for (let i = 0; i < myStr.length; i++) {
+//   if (isNaN(myStr[i])) {
+//     bag += myStr[i];
+//   } else {
+//     sum += Number(myStr[i]);
+//   }
+// }
+// //console.log(bag,sum)
 
 
 
-var aObj = { B: 25, A: 22 };
-var bObj = { D: 90, E: 30, A: 10 };
-//Output should be like this  {"A":32,"B":25,"D":90,"E":30}
+// var aObj = { B: 25, A: 22 };
+// var bObj = { D: 90, E: 30, A: 10 };
+// //Output should be like this  {"A":32,"B":25,"D":90,"E":30}
 
-let tempObj = {};
-for (let k in aObj) {
-  for (let l in bObj) {
-    if (k == l) {
-      tempObj[k] = aObj[k] + bObj[l];
-    } else {
-      tempObj[k] = aObj[k];
-      tempObj[l] = bObj[l];
-    }
-  }
-}
-console.log(JSON.stringify(tempObj));
+// let tempObj = {};
+// for (let k in aObj) {
+//   for (let l in bObj) {
+//     if (k == l) {
+//       tempObj[k] = aObj[k] + bObj[l];
+//     } else {
+//       tempObj[k] = aObj[k];
+//       tempObj[l] = bObj[l];
+//     }
+//   }
+// }
+// console.log(JSON.stringify(tempObj));
